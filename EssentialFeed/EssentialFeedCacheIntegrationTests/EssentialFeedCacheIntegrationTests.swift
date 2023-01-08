2827fcfe5c0cdf94687cc0086c2c77d3fd7753bc
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-01-08 17:51:52 +0300

    CoreDataFeedStore.retrieve() delivers empty on empty cache

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -7,9 +7,29 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
 
-    
-
+//    func test_load_deliversNoItemsOnEmptyCache() {
+//        let sut = makeSUT()
+//        
+//        let exp = expectation(description: "Wait for load completion")
+//        sut.load { result in
+//            switch result {
+//            case let .success(imageFeed):
+//                XCTAssertEqual(imageFeed, [], "Expected empty feed")
+//            case let .failure(error):
+//                XCTFail("Expected successful feed resut, got \(error) instead")
+//            }
+//            exp.fulfill()
+//        }
+//        wait(for: [exp], timeout: 1.0)
+//    }
+//    
+//    // MARK: - Helpers
+//    
+//    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+//        let store
+//    }
 }
