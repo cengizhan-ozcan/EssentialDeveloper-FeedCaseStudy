0ea041c65a82a78a08281b70b90f16b784ed012e
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-01-14 17:53:14 +0300

    simplify 'URLSessionHTTPClient' and 'CodableFeedStore' completion code with the new 'Result' APIs.

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/Codable/CodableFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/Codable/CodableFeedStore.swift
@@ -49,47 +49,40 @@ public class CodableFeedStore: FeedStore {
     public func retrieve(completion: @escaping RetrievalCompletion) {
         let storeURL = self.storeURL
         queue.async {
-            guard let data = try? Data(contentsOf: storeURL) else {
-                return completion(.success(.none))
-            }
-            do {
+            completion(Result {
+                guard let data = try? Data(contentsOf: storeURL) else {
+                    return nil
+                }
+                
                 let decoder = JSONDecoder()
                 let cache = try decoder.decode(Cache.self, from: data)
-                completion(.success(CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)))
-            } catch {
-                completion(.failure(error))
-            }
+                return CachedFeed(feed: cache.localFeed, timestamp: cache.timestamp)
+            })
         }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         let storeURL = self.storeURL
         queue.async(flags: .barrier) {
-            do {
+            completion(Result {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
                 let encoded = try encoder.encode(cache)
                 try encoded.write(to: storeURL)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         let storeURL = self.storeURL
         queue.async(flags: .barrier) {
-            guard FileManager.default.fileExists(atPath: storeURL.path) else {
-                return completion(.success(()))
-            }
-            
-            do {
+            completion(Result {
+                guard FileManager.default.fileExists(atPath: storeURL.path) else {
+                    return
+                }
+                
                 try FileManager.default.removeItem(at: storeURL)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
 }
