c856b9583e8c1ddc05b49298cb3b0ab40bbc3304
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-01-14 18:33:39 +0300

    Add empty project for prototype

---
+++ Prototype/Prototype/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Prototype
+//
+//  Created by Cengizhan Özcan on 14.01.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
