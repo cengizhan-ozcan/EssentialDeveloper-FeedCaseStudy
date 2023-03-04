d9f85460a25d5f2d6436dfe2a5a0f572e3ba8cc3
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-03-04 14:53:54 +0300

    Move 'FeedLoaderCacheDecorator' to production

---
+++ EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,28 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Cengizhan Özcan on 4.03.2023.
+//
+
+import EssentialFeed
+
+public class FeedLoaderCacheDecorator: FeedLoader {
+    
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map({ feed in
+                self?.cache.save(feed, completion: { _ in })
+                return feed
+            }))
+        }
+    }
+}
