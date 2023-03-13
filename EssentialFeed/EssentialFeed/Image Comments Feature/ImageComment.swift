37ac457fd8cbb9cdb3650c2627503cba9db1e717
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-03-13 21:06:51 +0300

    Add ImageComment data model

---
+++ EssentialFeed/EssentialFeed/Image Comments Feature/ImageComment.swift
@@ -0,0 +1,24 @@
+//
+//  ImageComment.swift
+//  EssentialFeed
+//
+//  Created by Cengizhan Özcan on 13.03.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+    
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
