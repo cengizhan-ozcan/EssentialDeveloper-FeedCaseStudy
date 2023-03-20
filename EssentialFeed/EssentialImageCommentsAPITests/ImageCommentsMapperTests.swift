d1a547e86c6dc8fd76b94f5d2fd3a4d6112ce51b
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-03-21 01:03:13 +0300

    Test ImageCommentMapper in isolation

--- EssentialFeed/EssentialImageCommentsAPITests/ImageCommentsMapperTests.swift
+++ EssentialFeed/EssentialImageCommentsAPITests/ImageCommentsMapperTests.swift
@@ -1,5 +1,5 @@
 //
-//  LoadImageCommentsFromRemoteUseCaseTests.swift
+//  ImageCommentsMapperTests.swift
 //  EssentialFeedAPITests
 //
 //  Created by Cengizhan Özcan on 13.03.2023.
@@ -10,102 +10,59 @@ import XCTest
 import EssentialFeed
 import EssentialImageCommentsAPI
 
-class LoadImageCommentsFromRemoteUseCaseTests: XCTestCase {
+class ImageCommentsMapperTests: XCTestCase {
     
-    func test_load_deliversErrorOnNon2xxHTTPResponse() {
-        let (sut, client) = makeSUT()
+    func test_map_throwsErrorOnNon2xxHTTPResponse() throws {
+        let json = makeItemsJSON([])
         let samples = [199, 150, 300, 400, 500]
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: failure(.invalidData)) {
-                let json = makeItemsJSON([])
-                client.complete(withStatusCode: code, data: json, at: index)
-            }
+        
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
     
-    func test_load_deliversErrorOn2xxHTTPResponseWithInvalidJSON() {
-        let (sut, client) = makeSUT()
-        
+    func test_map_throwsErrorOn2xxHTTPResponseWithInvalidJSON() throws {
+        let invalidJSON = Data("invalid json".utf8)
         let samples = [200, 201, 250, 288, 299]
         
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: failure(.invalidData)) {
-                let invalidJSON = Data("invalid json".utf8)
-                client.complete(withStatusCode: code, data: invalidJSON, at: index)
-            }
+        try samples.forEach { code in
+            XCTAssertThrowsError(
+                try ImageCommentsMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: code))
+            )
         }
     }
     
-    func test_load_deliversNoItemOn2xxHTTPResponseWithEmptyJSONList() {
-        let (sut, client) = makeSUT()
-
+    func test_map_deliversNoItemOn2xxHTTPResponseWithEmptyJSONList() throws {
+        let json = makeItemsJSON([])
         let samples = [200, 201, 250, 288, 299]
         
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: .success([])) {
-                let emptyListJSON = makeItemsJSON([])
-                client.complete(withStatusCode: code, data: emptyListJSON, at: index)
-            }
+        try samples.forEach { code in
+            let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            XCTAssertEqual(result, [])
         }
         
     }
     
-    func test_load_deliversItemsOn2xxHTTPResponseWithJSONItems() {
-        let (sut, client) = makeSUT()
-        
+    func test_map_deliversItemsOn2xxHTTPResponseWithJSONItems() throws {
         let item1 = makeItem(id: UUID(), message: "a message",
                              createdAt: (Date(timeIntervalSince1970: 1598627222), "2020-08-28T15:07:02+00:00"),
                              username: "a username")
         let item2 = makeItem(id: UUID(), message: "another message",
                              createdAt: (Date(timeIntervalSince1970: 1577881882), "2020-01-01T12:31:22+00:00"),
                              username: "another username")
-        let items = [item1.model, item2.model]
+        let json = makeItemsJSON([item1.json, item2.json])
         
         let samples = [200, 201, 250, 288, 299]
         
-        samples.enumerated().forEach { index, code in
-            expect(sut, toCompleteWithResult: .success(items)) {
-                let json = makeItemsJSON([item1.json, item2.json])
-                client.complete(withStatusCode: code, data: json, at: index)
-            }
+        try samples.forEach { code in
+            let result = try ImageCommentsMapper.map(json, from: HTTPURLResponse(statusCode: code))
+            XCTAssertEqual(result, [item1.model, item2.model])
         }
     }
     
     // MARK: - Helpers
-    
-    private func makeSUT(url: URL = URL(string: "https://a-url.com")!,
-                         file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteImageCommentsLoader, client: HTTPClientSpy) {
-        let client = HTTPClientSpy()
-        let sut = RemoteImageCommentsLoader(url: url, client: client)
-        trackForMemoryLeaks(sut, file: file, line: line)
-        trackForMemoryLeaks(client, file: file, line: line)
-        return (sut, client)
-    }
-    
-    private func failure(_ error: RemoteImageCommentsLoader.Error) -> RemoteImageCommentsLoader.Result {
-        return .failure(error)
-    }
-    
-    private func expect(_ sut: RemoteImageCommentsLoader, toCompleteWithResult expectedResult: RemoteImageCommentsLoader.Result,
-                        action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
-        
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { receivedResult in
-            switch(receivedResult, expectedResult) {
-            case let(.success(receivedItems), .success(expectedItems)):
-                XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-            case let(.failure(receivedError as RemoteImageCommentsLoader.Error), .failure(expectedError as RemoteImageCommentsLoader.Error)):
-                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
-            default:
-                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-        
-        action()
-        
-        wait(for: [exp], timeout: 1.0)
-    }
     
     private func makeItem(id: UUID, message: String, createdAt: (date: Date, iso8601String: String), username: String) -> (model: ImageComment, json: [String:Any]) {
         let item = ImageComment(id: id, message: message, createdAt: createdAt.date, username: username)
