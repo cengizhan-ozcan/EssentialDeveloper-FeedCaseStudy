62735ae8f3632cd5f56d138263c28b224617dc86
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-05-20 12:06:58 +0300

    Make LoadResourcePresenter generic over the Resource types

--- EssentialFeed/SharedPresentation/LoadResourcePresenter.swift
+++ EssentialFeed/SharedPresentation/LoadResourcePresenter.swift
@@ -11,19 +11,22 @@ import EssentialFeed
 import EssentialFeedPresentation
 
 public protocol ResourceView {
-    func display(_ viewModel: String)
+    
+    associatedtype ResourceViewModel
+    
+    func display(_ viewModel: ResourceViewModel)
 }
 
-public class LoadResourcePresenter {
+public class LoadResourcePresenter<Resource, View: ResourceView> {
     
-    public typealias Mapper = (String) -> String
+    public typealias Mapper = (Resource) -> View.ResourceViewModel
     
-    private let resourceView: ResourceView
+    private let resourceView: View
     private let loadingView: FeedLoadingView
     private let errorView: FeedErrorView
     private let mapper: Mapper
     
-    public init(resourceView: ResourceView, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
+    public init(resourceView: View, loadingView: FeedLoadingView, errorView: FeedErrorView, mapper: @escaping Mapper) {
         self.resourceView = resourceView
         self.loadingView = loadingView
         self.errorView = errorView
@@ -40,10 +43,10 @@ public class LoadResourcePresenter {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
     
-    public func didFinishLoading(with resource: String) {
+    public func didFinishLoading(with resource: Resource) {
         resourceView.display(mapper(resource))
         loadingView.display(FeedLoadingViewModel(isLoading: false))
-    }
+    } 
     
     public func didFinishLoadingFeed(with error: Error) {
         errorView.display(.error(message: feedLoadError))
