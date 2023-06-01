d1e187a9222ad5b0256ef58e67a00f537f1a4d48
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-06-01 21:51:49 +0300

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/IImageComments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Cengizhan Özcan on 1.06.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+final class ImageCommentCell: UITableViewCell {
+    
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var messageLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    
+}
