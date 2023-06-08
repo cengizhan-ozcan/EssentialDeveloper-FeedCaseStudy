9fcc988a27afe1ae786c398d71791f180e03d171
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-06-08 21:36:55 +0300

    Move localization helpers to shared scope

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -7,6 +7,9 @@
 
 import Foundation
 import EssentialFeed
+import EssentialFeedPresentation
+import EssentialImageCommentPresentation
+import SharedPresentation
 
 func anyError() -> NSError {
     return NSError(domain: "any error", code: 0)
@@ -22,4 +25,20 @@ func anyData() -> Data {
 
 func uniqueFeed() -> [FeedImage] {
     return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+}
+
+private class DummyView: ResourceView {
+    func display(_ viewModel: Any) {}
+}
+
+var loadError: String {
+    LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    FeedPresenter.title
+}
+
+var commentsTitle: String {
+    ImageCommentPresenter.title
 }
