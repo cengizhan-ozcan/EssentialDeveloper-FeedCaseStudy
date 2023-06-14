bf6202422b483717f37412481df99420dfbbed2c
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-06-15 00:17:57 +0300

    Add "Load More" cell controller with loading indicator

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controller/LoadMoreCellController.swift
@@ -0,0 +1,30 @@
+//
+//  LoadMoreCellController.swift
+//  EssentialFeediOS
+//
+//  Created by Cengizhan Özcan on 14.06.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import UIKit
+import SharedPresentation
+
+public final class LoadMoreCellController: NSObject, UITableViewDataSource {
+    
+    private let cell = LoadMoreCell()
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        cell
+    }
+}
+
+extension LoadMoreCellController: ResourceLoadingView {
+    
+    public func display(_ viewModel: ResourceLoadingViewModel) {
+        cell.isLoading = viewModel.isLoading
+    }
+}
---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/LoadMoreCell.swift
@@ -0,0 +1,35 @@
+//
+//  LoadMoreCell.swift
+//  EssentialFeediOS
+//
+//  Created by Cengizhan Özcan on 15.06.2023.
+//  Copyright © 2023 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public class LoadMoreCell: UITableViewCell {
+    
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        contentView.addSubview(spinner)
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            contentView.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
+        ])
+        return spinner
+    }()
+    
+    public var isLoading: Bool {
+        get { spinner.isAnimating }
+        set {
+            if newValue {
+                spinner.startAnimating()
+            } else {
+                spinner.stopAnimating()
+            }
+        }
+    }
+}
