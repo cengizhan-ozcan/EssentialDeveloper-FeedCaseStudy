83afe8fc90049a648645a966b2ba0e5dbd2fd556
Cengizhan Özcan <cengizhan.ozcan@ozu.edu.tr>
2023-06-18 19:36:17 +0300

    loads and cancels requests

--- EssentialApp/EssentialAppTests/RemoteFeedImageDataLoaderAdapterTests.swift
+++ EssentialApp/EssentialAppTests/RemoteFeedImageDataLoaderAdapterTests.swift
@@ -9,12 +9,19 @@ import XCTest
 import EssentialFeed
 import SharedAPI
 
-class RemoteFeedImageDataLoaderAdapter {
+class RemoteFeedImageDataLoaderAdapter: FeedImageDataLoader {
     
     let client: HTTPClient
     
     init(client: HTTPClient) {
         self.client = client
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> LoaderTask {
+        let remoteLoader = RemoteLoader(url: url, client: client) { data,response in
+            return data
+        }
+        return remoteLoader.load(completion: completion)
     }
 }
 
@@ -24,6 +31,25 @@ class RemoteFeedImageDataLoaderAdapterTests: XCTestCase {
         let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty, "Expected no loaded URLs in the loader")
+    }
+    
+    func test_loadImageData_loadsFromLoader() {
+        let (sut, client) = makeSUT()
+        let url = anyURL()
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
+    func test_loadImageData_cancelsLoaderTask() {
+        let (sut, client) = makeSUT()
+        let url = anyURL()
+        
+        let task = sut.loadImageData(from: anyURL(), completion: { _ in })
+        task.cancel()
+        
+        XCTAssertEqual(client.cancelledURLs, [url])
     }
     
     
@@ -38,17 +64,19 @@ class RemoteFeedImageDataLoaderAdapterTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
         
         var requestedURLs = [URL]()
+        var cancelledURLs = [URL]()
         
-        private class Task: HTTPClientTask {
+        private struct Task: HTTPClientTask {
             
-            func cancel() {
-                
-            }
+            let callback: () -> Void
+            func cancel() { callback() }
         }
         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
             requestedURLs.append(url)
-            return Task()
+            return Task { [weak self, url] in
+                self?.cancelledURLs.append(url)
+            }
         }
     }
 }
